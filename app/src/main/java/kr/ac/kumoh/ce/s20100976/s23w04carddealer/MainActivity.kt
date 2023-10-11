d84fcdc342ea60463babb0b161e6ebca5a4c216c
이승훈 <hun2548@naver.com>
2023-10-11 16:18:58 +0900

    s23w04carddealer 5장 가로 세로 랜덤

--- app/src/main/java/kr/ac/kumoh/ce/s20100976/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20100976/s23w04carddealer/MainActivity.kt
@@ -3,44 +3,96 @@ package kr.ac.kumoh.ce.s20100976.s23w04carddealer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20100976.s23w04carddealer.databinding.ActivityMainBinding
 import kotlin.random.Random
 
+
 class MainActivity : AppCompatActivity() {
-    private  lateinit var  main: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var main: ActivityMainBinding
+    private lateinit var model: CardDealerViewModel
+
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
-
+        //setContentView(R.layout.activity_main)
         setContentView(main.root)
+       model = ViewModelProvider(this)[CardDealerViewModel::class.java]
 
-        val card = Random.nextInt(52)
-        // val card = 33
-        // Log.i("card!!!", "$card: ${getCardNumber(card)}")
-        val res = resources.getIdentifier(
-            getCardNumber(card),
-            "drawable",
-            packageName
-        )
-        main.card1.setImageResource(res)
+        //binding.card1.setImageResource(R.drawable.c_2_of_hearts)
+//        val c = Random.nextInt(52)
+//        Log.i("Test", "$c : ${getCardName(c)}")
+       model.cards.observe(this, Observer{
+           val res = IntArray(5)
+           for (i in it.indices) {
+               res[i] = resources.getIdentifier(
+                   getCardName(it[i]),
+                   "drawable",
+                   packageName
+               )
+           }
+           main.card1.setImageResource(res[0])
+           main.card2.setImageResource(res[1])
+           main.card3.setImageResource(res[2])
+           main.card4.setImageResource(res[3])
+           main.card5.setImageResource(res[4])
+       })
+
+       main.btnShuffle.setOnClickListener{
+           model.shuffle()
+       }
+
+//       // TODO: 하드 코딩 없앨 것
+//       val card = 25
+//       val res = resources.getIdentifier(
+//            getCardName(card),
+//            "drawable",
+//            packageName
+//        )
+//
+//        main.card1.setImageResource(res)
+//        main.card2.setImageResource(res)
+//        main.card3.setImageResource(res)
+//        main.card4.setImageResource(res)
+//        main.card5.setImageResource(res)
     }
 
-    private fun getCardNumber(c: Int): String{
-        val shape = when (c/13){
+    private fun getCardName(c: Int) : String {
+        // val에서 var로 변경
+        var shape = when (c / 13) {
             0 -> "spades"
             1 -> "diamonds"
             2 -> "hearts"
             3 -> "clubs"
             else -> "error"
         }
-        val number = when (c%13){
+
+        val number = when (c % 13) {
             0 -> "ace"
-            in 1..9 -> (c%13 +1).toString()
-            10 -> "jack"
-            11 -> "queen"
-            12 -> "king"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> {
+                shape = shape.plus("2")
+                "jack"
+            }
+            11 -> {
+                shape = shape.plus("2")
+                "queen"
+            }
+            12 -> {
+                shape = shape.plus("2")
+                "king"
+            }
             else -> "error"
         }
-        return "c_${number}_of_$shape"
+
+        // 이 방법이 더 나을 듯
+//        if (c % 13 in 10..12)
+//            return "c_${number}_of_${shape}2"
+//        else
+//            return "c_${number}_of_${shape}"
+
+        return "c_${number}_of_${shape}"
     }
 }
